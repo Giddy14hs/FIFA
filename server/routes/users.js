946745121a447d907393333f990a7cca80fb6a91
lemis <lemisogideon@gmail.com>
2024-08-19 21:46:10 +0300

    safe

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 import {Router} from "express"
 import {login, signup } from "../controllers/users.js";
 
-const router = Router();
+const router = Router()
 
 router.post("/login", login);
 router.post("/signup", signup)
