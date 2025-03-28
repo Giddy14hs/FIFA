491aea4780f81edc199841b2e909e1e0b03be86c
lemis <lemisogideon@gmail.com>
2025-03-28 15:36:55 +0300

    build+error+solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/navigation/Navigation';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import BenevolentForm from './components/form/formLoans';
-import Login from './components/auth/Login';
-import Signup from './components/auth/Signup';
-import GoogleLogin from './components/auth/GoogleLogin';
-import PrivacyPolicy from './components/privacyPolicy/privacyPolicy';
+import AppRouter from './router/AppRouter';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/form" element={<BenevolentForm />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/google-login" element={<GoogleLogin />} />
-          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <AppRouter />
+    </div>
   );
 }
 
